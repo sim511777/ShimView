447af238b88ef8bdd8781aab089a1b25f8efd07d
sim511777 <sim51177@gmail.com>
2020-01-18 15:50:33 +0900

    배경컬러 변경

--- ShimView/FormMain.Designer.cs
+++ ShimView/FormMain.Designer.cs
@@ -29,6 +29,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "FormMain";
             this.Text = "Shim View";
