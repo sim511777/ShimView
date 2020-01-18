5a02f929148244d1a9a83340d08f5d2031b4bf4e
sim511777 <sim51177@gmail.com>
2020-01-18 15:39:18 +0900

    메인폼 클래스, 타이틀 변경

--- ShimView/FormMain.Designer.cs
+++ ShimView/FormMain.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace ShimView {
-    partial class Form1 {
+    partial class FormMain {
         /// <summary>
         /// 필수 디자이너 변수입니다.
         /// </summary>
@@ -23,10 +23,17 @@
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FormMain";
+            this.Text = "Shim View";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
